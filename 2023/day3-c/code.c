e236ea1f9b984d232fcf4630d5ffad83da0c9e87
Ahmed0427 <ahmedabdallah1090@gmail.com>
2025-01-24 18:27:42 +0200

    finish day 3 in c

--- 2023/day3-c/code.c
+++ 2023/day3-c/code.c
@@ -5,16 +5,20 @@
 #define LINE_LEN_MAX 256
 #define LINES_CNT_MAX 256
 
+char grid[LINES_CNT_MAX][LINE_LEN_MAX];
+long long gearsValue[LINES_CNT_MAX * LINE_LEN_MAX][2];
+
 int isDigit(char c) {
     return (c >= '0' && c <= '9');
 }
 
-int isValidPosition(int x, int y, char grid[LINES_CNT_MAX][LINE_LEN_MAX]) {
+int isValidPosition(int x, int y) {
     int directions[8][2] = {
         {1, 0}, {-1, 0}, {0, 1}, {0, -1},    
         {1, 1}, {-1, -1}, {1, -1}, {-1, 1}  
     };
 
+    int valid = 0;
     for (int i = 0; i < 8; i++) {
         int nx = x + directions[i][0];
         int ny = y + directions[i][1];
@@ -24,37 +28,80 @@ int isValidPosition(int x, int y, char grid[LINES_CNT_MAX][LINE_LEN_MAX]) {
         }
 
         if (!isDigit(grid[nx][ny]) && grid[nx][ny] != '.' && grid[nx][ny] != '\n') {
-            return 1;
+            valid = 1;
         }
     }
 
-    return 0;
+    return valid;
 }
 
-int sumValidNumbers(char grid[LINES_CNT_MAX][LINE_LEN_MAX]) {
+int isNearStar(int x, int y) {
+    int directions[8][2] = {
+        {1, 0}, {-1, 0}, {0, 1}, {0, -1},    
+        {1, 1}, {-1, -1}, {1, -1}, {-1, 1}  
+    };
+
+    int valid = 0;
+    for (int i = 0; i < 8; i++) {
+        int nx = x + directions[i][0];
+        int ny = y + directions[i][1];
+
+        if (nx < 0 || ny < 0 || nx >= LINES_CNT_MAX || ny >= LINE_LEN_MAX) {
+            continue;
+        }
+
+        if (grid[nx][ny] == '*') {
+            return (nx * LINE_LEN_MAX) + ny;
+        }
+    }
+
+    return -1;
+}
+
+long long sumValidNumbers() {
     int rowCount = LINES_CNT_MAX;
     int colCount = LINE_LEN_MAX;
 
-    int sum = 0;
+    long long sum = 0;
     for (int i = 0; i < rowCount; i++) {
-        int num = 0, valid = 0;
+        int num = 0, valid = 0, gearPos = -1;
         for (int j = 0; j < colCount; j++) {
             if (isDigit(grid[i][j])) {
                 num = num * 10 + (grid[i][j] - '0');
-                if (isValidPosition(i, j, grid)) {
-                    valid = 1;
-                }
+                if (isValidPosition(i, j)) valid = 1;
+                gearPos = gearPos == -1 ? isNearStar(i, j) : gearPos;
             }
             else {
                 if (valid) {
                     sum += num;  
                 }
+                if (gearPos != -1) {
+                    if (gearsValue[gearPos][0] == 0) gearsValue[gearPos][0] = 1;
+                    gearsValue[gearPos][0] *= num;
+                    gearsValue[gearPos][1]++;
+                }
                 valid = 0;  
+                gearPos = -1;
                 num = 0;   
             }
         }
     }
 
+    return sum;
+}
+
+long long part1() {
+    return sumValidNumbers();
+}
+
+long long part2() {
+    long long sum = 0;
+    int n = LINES_CNT_MAX * LINE_LEN_MAX;
+    for (int i = 0; i < n; i++) {
+        if (gearsValue[i][1] == 2) {
+            sum += gearsValue[i][0];
+        }
+    }
     return sum;
 }
 
@@ -73,16 +120,18 @@ int main(int argc, char *argv[]) {
 
      int lineCnt = 0;
      char line[LINE_LEN_MAX];
-     char grid[LINES_CNT_MAX][LINE_LEN_MAX];
 
      memset(grid, '.', sizeof(grid));
+     memset(gearsValue, 0, sizeof(gearsValue));
 
      while (fgets(line, sizeof(line), file)) {
         strcpy(grid[lineCnt++], line);
      }
 
-     long long result = sumValidNumbers(grid);
-     printf("Part 1 result: %lld\n", result);
+     long long ans1 = part1();
+     long long ans2 = part2();
+     printf("Part 1: %lld\n", ans1);
+     printf("Part 2: %lld\n", ans2);
 
      fclose(file);
      return 0;
