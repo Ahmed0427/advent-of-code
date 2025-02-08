0ff80d66cd8de3542a019a83ef07cd898afe9e3e
Ahmed0427 <ahmedabdallah1090@gmail.com>
2025-02-08 22:24:08 +0200

    feat: day 5 part 2 in C

--- 2023/day5-c/main.c
+++ 2023/day5-c/main.c
@@ -4,6 +4,35 @@
 #include <stdint.h>
 #include <ctype.h>
 
+#define MAX_SEEDS 32 
+#define MAX_LINE_LENGTH 256
+#define MAX_MAP_ENTRIES 64 
+
+#define max(a, b) (((a) > (b)) ? a : b)
+#define min(a, b) (((a) < (b)) ? a : b)
+
+typedef struct {
+    int64_t dest_start;
+    int64_t src_start;
+    int64_t range_length;
+} MapEntry;
+
+typedef struct {
+    MapEntry entries[MAX_MAP_ENTRIES];
+    int size;
+} Map;
+
+typedef struct {
+    int64_t start;
+    int64_t end;
+} Interval;
+
+typedef struct {
+    Interval* list;
+    int capacity;
+    int size;
+} IntervalList;
+
 void get_seeds(char* line, int64_t* seeds, int64_t* seeds_count) {
     line = strchr(line, ':') + 2;  
 
@@ -12,13 +41,12 @@ void get_seeds(char* line, int64_t* seeds, int64_t* seeds_count) {
         if (isdigit(*line)) {
             if (num == -1) {
                 num = (*line) - '0';
-            }
-            else {
+            } else {
                 num = num * 10 + (*line) - '0';
             }
         } else {
             if (num != -1) {
-                if (*seeds_count < 254) {  
+                if (*seeds_count < MAX_SEEDS) {  
                     seeds[(*seeds_count)++] = num;
                 }
                 num = -1;
@@ -26,99 +54,198 @@ void get_seeds(char* line, int64_t* seeds, int64_t* seeds_count) {
         }
         line++;
     }
-}
-
-void get_section(int64_t sec[50][3], FILE* file_d) {
-    int64_t i = 0;
-    char line[100];
-    while (fgets(line, sizeof(line), file_d) && line[0] != '\n') {
-        if (sscanf(line, "%ld %ld %ld", &sec[i][0], &sec[i][1], &sec[i][2]) == 3) {
-            i++;
+    if (num != -1) {
+        if (*seeds_count < MAX_SEEDS) {  
+            seeds[(*seeds_count)++] = num;
         }
     }
 }
 
-int64_t getDest(int64_t src, int64_t map[50][3]) {
-    for (int64_t i = 0; i < 50; i++) {
-        if (map[i][2] == 0) continue;
-        if (map[i][1] > src) continue;
-        if (map[i][1] + map[i][2] <= src) continue;
-        return map[i][0] + (src - map[i][1]);
+void parse_map(FILE* file, Map* map) {
+    map->size = 0;
+    char line[MAX_LINE_LENGTH];
+    while (fgets(line, sizeof(line), file) && line[0] != '\n') {
+        MapEntry* entry = &map->entries[map->size];
+        if (sscanf(line, "%ld %ld %ld", &entry->dest_start,
+        &entry->src_start, &entry->range_length) == 3) {
+            map->size++;
+        }
+    }
+}
+
+int64_t map_value(int64_t src, Map* map) {
+    for (int i = 0; i < map->size; i++) {
+        MapEntry* entry = &map->entries[i];
+        if (src >= entry->src_start && src < entry->src_start + entry->range_length) {
+            return entry->dest_start + (src - entry->src_start);
+        }
     }
     return src;
 }
 
-int main(int argc, char** argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-        exit(1);
+int64_t solve_part1(int64_t seeds[MAX_SEEDS], int seeds_count, Map maps[7]) {
+    int64_t result = INT64_MAX;
+    for (int i = 0; i < seeds_count; i++) {
+        int64_t location = seeds[i]; 
+        for (int j = 0; j < 7; j++) {
+            location = map_value(location, &maps[j]);
+        }
+        if (result > location) result = location;
     }
+    return result;
+}
 
-    FILE* file_d = fopen(argv[1], "r");
-
-    if (!file_d) {
-        perror("Couldn't open file");
-        exit(1);
+IntervalList make_intervals(int64_t seeds[MAX_SEEDS], int seeds_count) {
+    IntervalList result = {NULL, 0, 0};
+    for (int i = 0; i < seeds_count; i += 2) {
+        Interval interval;
+        interval.start = seeds[i];
+        interval.end = seeds[i] + seeds[i + 1] - 1;
+        if (result.size >= result.capacity) {
+            result.capacity = (result.capacity) ? result.capacity * 2 : 4; 
+            result.list = realloc(result.list, result.capacity * sizeof(Interval));
+        }  
+        result.list[result.size++] = interval;
     }
+    return result;
+}
 
-    char line[256];
-    fgets(line, sizeof(line), file_d);  
+IntervalList update_intervals(IntervalList* input, Map map) {
+    IntervalList current = *input;
+    IntervalList result = {NULL, 0, 0};
 
-    int64_t seeds[20];
-    int64_t seeds_count = 0;
-    get_seeds(line, seeds, &seeds_count);
+    for (int j = 0; j < map.size; j++) {
+        MapEntry* entry = &map.entries[j];
+        int64_t src_start = entry->src_start;
+        int64_t src_end = src_start + entry->range_length - 1;
+        int64_t dest_start = entry->dest_start;
 
-    fgets(line, sizeof(line), file_d);  
+        IntervalList new_current = {NULL, 0, 0};
+        for (int i = 0; i < current.size; i++) {
+            Interval curr = current.list[i];
 
-    int64_t seed_map[50][3];
-    int64_t soil_map[50][3];
-    int64_t fert_map[50][3];
-    int64_t water_map[50][3];
-    int64_t light_map[50][3];
-    int64_t temp_map[50][3];
-    int64_t humid_map[50][3];
-
-    
-    memset(seed_map, 0, sizeof(seed_map));
-    memset(soil_map, 0, sizeof(soil_map));
-    memset(fert_map, 0, sizeof(fert_map));
-    memset(water_map, 0, sizeof(water_map));
-    memset(light_map, 0, sizeof(light_map));
-    memset(temp_map, 0, sizeof(temp_map));
-    memset(humid_map, 0, sizeof(humid_map));
-
-    
-    while (fgets(line, sizeof(line), file_d)) {
-        switch (line[0]) {
-            case 's':
-                switch (line[1]) {
-                    case 'e': get_section(seed_map, file_d); break;
-                    case 'o': get_section(soil_map, file_d); break;
+            if (curr.end < src_start || curr.start > src_end) {
+                if (new_current.size >= new_current.capacity) {
+                    new_current.capacity = new_current.capacity ? new_current.capacity * 2 : 4;
+                    new_current.list = realloc(new_current.list, new_current.capacity * sizeof(Interval));
                 }
-                break;
+                new_current.list[new_current.size++] = curr;
+                continue;
+            }
 
-            case 'f': get_section(fert_map, file_d); break;
-            case 'w': get_section(water_map, file_d); break;
-            case 'l': get_section(light_map, file_d); break;
-            case 't': get_section(temp_map, file_d); break;
-            case 'h': get_section(humid_map, file_d); break;
+            int64_t overlap_start = max(curr.start, src_start);
+            int64_t overlap_end = min(curr.end, src_end);
+
+            Interval mapped = {
+                dest_start + (overlap_start - src_start),
+                dest_start + (overlap_end - src_start)
+            };
+            if (result.size >= result.capacity) {
+                result.capacity = result.capacity ? result.capacity * 2 : 4;
+                result.list = realloc(result.list, result.capacity * sizeof(Interval));
+            }
+            result.list[result.size++] = mapped;
+
+            if (curr.start < overlap_start) {
+                Interval pre = {curr.start, overlap_start - 1};
+                if (new_current.size >= new_current.capacity) {
+                    new_current.capacity = new_current.capacity ? new_current.capacity * 2 : 4;
+                    new_current.list = realloc(new_current.list, new_current.capacity * sizeof(Interval));
+                }
+                new_current.list[new_current.size++] = pre;
+            }
+
+            if (curr.end > overlap_end) {
+                Interval post = {overlap_end + 1, curr.end};
+                if (new_current.size >= new_current.capacity) {
+                    new_current.capacity = new_current.capacity ? new_current.capacity * 2 : 4;
+                    new_current.list = realloc(new_current.list, new_current.capacity * sizeof(Interval));
+                }
+                new_current.list[new_current.size++] = post;
+            }
+        }
+
+        free(current.list);
+        current = new_current;
+    }
+
+    for (int i = 0; i < current.size; i++) {
+        if (result.size >= result.capacity) {
+            result.capacity = result.capacity ? result.capacity * 2 : 4;
+            result.list = realloc(result.list, result.capacity * sizeof(Interval));
+        }
+        result.list[result.size++] = current.list[i];
+    }
+
+    free(current.list);
+    return result;
+}
+
+int64_t solve_part2(int64_t seeds[MAX_SEEDS], int seeds_count, Map maps[7]) {
+    IntervalList intervals = make_intervals(seeds, seeds_count);
+
+    for (int i = 0; i < 7; i++) {
+        IntervalList updated = update_intervals(&intervals, maps[i]);
+        intervals = updated;
+    }
+
+    int64_t result = INT64_MAX;
+    for (int i = 0; i < intervals.size; i++) {
+        if (intervals.list[i].start < result) {
+            result = intervals.list[i].start;
         }
     }
 
-    int64_t ans1 = 2e9;
-    for (int64_t i = 0; i < seeds_count; i++) {
-        int64_t seed_dist = getDest(seeds[i], seed_map);
-        int64_t soil_dist = getDest(seed_dist, soil_map);
-        int64_t fert_dist = getDest(soil_dist, fert_map);
-        int64_t water_dist = getDest(fert_dist, water_map);
-        int64_t light_dist = getDest(water_dist, light_map);
-        int64_t temp_dist = getDest(light_dist, temp_map);
-        int64_t humid_dist = getDest(temp_dist, humid_map);
-        ans1 = (ans1 < humid_dist) ? ans1 : humid_dist;
+    free(intervals.list);
+    return result;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
+    FILE* file = fopen(argv[1], "r");
+
+    if (!file) {
+        perror("Error opening file");
+        return 1;
     }
 
-    printf("ans1: %ld\n", ans1);
+    char line[MAX_LINE_LENGTH];
+    int64_t seeds[MAX_SEEDS];
+    int64_t seeds_count = 0;
 
-    fclose(file_d);
+    fgets(line, sizeof(line), file);
+    get_seeds(line, seeds, &seeds_count);
+    fgets(line, sizeof(line), file);
+
+    Map maps[7] = {0};
+    const char* map_headers[] = {
+        "seed-to-soil", 
+        "soil-to-fertilizer", 
+        "fertilizer-to-water", 
+        "water-to-light", 
+        "light-to-temperature", 
+        "temperature-to-humidity", 
+        "humidity-to-location"
+    };
+
+    for (int i = 0; i < 7; i++) {
+        while (fgets(line, sizeof(line), file)) {
+            if (strstr(line, map_headers[i])) {
+                parse_map(file, &maps[i]);
+                break;
+            }
+        }
+    }
+
+    int64_t ans1 = solve_part1(seeds, seeds_count, maps);
+    int64_t ans2 = solve_part2(seeds, seeds_count, maps);
+
+    printf("part 1: %ld\n", ans1);
+    printf("part 2: %ld\n", ans2);
+
+    fclose(file);
     return 0;
 }
